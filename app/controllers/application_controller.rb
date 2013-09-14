9197780585c6c8b0adf1ac15f01b065e7f96f3f8
benomesi <benomesi@gmail.com>
2013-09-14 04:13:23 -0400

    Tons of front-end changes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up){ |u| u.permit(:email, :password, :password_confirmation) }
   end
+
 end
+
