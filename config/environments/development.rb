05971407bfa1f552416f4e81deef545dd3b3ef65
benomesi <benomesi@gmail.com>
2013-09-18 17:30:36 -0400

    CSS and bug fixes
    Updated CSS
    Fixed localhost issues with forgot password

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ SecretSanta::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+
+  #forgot password 
+  config.action_mailer.default_url_options = { :host => 'localhost' }
 end
